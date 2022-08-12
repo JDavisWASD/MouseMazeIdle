42fd44e7ff8b70fe3b726c5ff4fa1cbe6670c9bc
JDavisWASD <jdavisWASD@gmail.com>
2022-08-12 05:41:03 -0700

    fixed overwriting start tile

--- maze.js
+++ maze.js
@@ -11,43 +11,45 @@ function buildMaze(height, width) {
          3D. Add adjacent cells to the list of walls
      3E. Remove the random wall from the list of walls */
 
-    let grid = [];  //Maybe rename to maze?
-//    let maze = new Set();
+    let maze = [];
+    let paths = new Set();
     let walls = new Set();  //Maybe rename to possibleCells?
     let iteratable = null;
     let randomIndex = null;
     let currentCell = null;
     let adjacentCellCount = null;
 
-    for (let x = 0; x < width; x++) {
-        grid[x] = [];
-        for (let y = 0; y < height; y++) {
+    for (let y = 0; y < height; y++) {
+        maze[y] = [];
+        for (let x = 0; x < width; x++) {
             let cell = {
                 x: x,
                 y: y,
                 status: 'wall',
                 adjacentCells: []
             };
-            grid[x][y] = cell;
-            if (grid[x - 1] && grid[x - 1][y]) {
-                let west = grid[x - 1][y];
+            maze[y][x] = cell;
+            if (maze[y - 1] && maze[y - 1][x]) {
+                let west = maze[y - 1][x];
                 cell.adjacentCells.push(west);
                 west.adjacentCells.push(cell);
             }
 
-            if (grid[x][y - 1]) {
-                let south = grid[x][y - 1];
+            if (maze[y][x - 1]) {
+                let south = maze[y][x - 1];
                 cell.adjacentCells.push(south);
                 south.adjacentCells.push(cell);
             }
         }
     }
 
-    let startingX = Math.floor(Math.random() * grid.length);
-    let startingY = Math.floor(Math.random() * grid[startingX].length);
-    let startingCell = grid[startingX][startingY];
-    startingCell.status = 'path';
-//    maze.add(startingCell);
+    let startingY = Math.floor(Math.random() * maze.length);
+    let startingX = Math.floor(Math.random() * maze[startingY].length);
+//    let startingCell = maze[maze.length-1][0];
+    let startingCell = maze[startingY][startingX];
+    startingCell.status = 'mouse';
+//    startingCell.status = 'path';
+    paths.add(startingCell);
     for (let i = 0; i < startingCell.adjacentCells.length; i++) {
         walls.add(startingCell.adjacentCells[i]);
     }
@@ -58,13 +60,15 @@ function buildMaze(height, width) {
         currentCell = iteratable[randomIndex];
         adjacentCellCount = 0;
         for (let i = 0; i < currentCell.adjacentCells.length; i++) {
-            if (currentCell.adjacentCells[i].status == 'path') {
+            if (currentCell.adjacentCells[i].status == 'path' ||
+                    currentCell.adjacentCells[i].status == 'mouse') {
                 adjacentCellCount++;
             }
         }
 
-        if (adjacentCellCount == 1) {
+        if (adjacentCellCount == 1 && currentCell.status != 'mouse') {
             currentCell.status = 'path';
+            paths.add(currentCell);
             for (let i = 0; i < currentCell.adjacentCells.length; i++) {
                 walls.add(currentCell.adjacentCells[i]);
             }
@@ -72,19 +76,34 @@ function buildMaze(height, width) {
 
         walls.delete(currentCell);
     }
-    return grid;
+    return maze;
 }
 
 function drawMaze(maze) {
-    output = '';
-    for (let x = 0; x < maze.length; x++) {
-        output += "<div class='column'>";
-        for (let y = 0; y < maze[x].length; y++) {
-            output += "<div class='" + maze[x][y].status + "'></div>";
-        }
+    output = "<div class='column'>";
+    for (let y = 0; y < maze.length + 2; y++) {
+        output += "<div class='wall'></div>";
+    }
+    output += '</div>';
 
-        output += '</div>';
+    for (y = 0; y < maze.length; y++) {
+        output += "<div class='column'><div class='wall'></div>";
+        for (let x = 0; x < maze[y].length; x++) {
+            output += "<div class='" + maze[y][x].status + "'></div>";
+        }
+        output += "<div class='wall'></div></div>"
     }
 
+    output += "<div class='column'>";
+    for (y = 0; y < maze.length + 2; y++) {
+        output += "<div class='wall'></div>";
+    }
+    output += '</div>';
     document.getElementById('maze').innerHTML = output;
+}
+
+function spawnMouse(paths) {
+}
+
+function spawnCheese(paths) {
 }
